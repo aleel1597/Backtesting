0caa783ea4c4b6347e1a7906d9467671293fef2a
Alan Lee <Alan@Changyings-iMac.local>
2019-12-21 22:52:30 -0500

    new backtest

--- alpaca_backtrader_api/main.py
+++ alpaca_backtrader_api/main.py
@@ -1,42 +1,43 @@
 import backtrader as bt
 import alpaca_backtrader_api
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt # no error, it works
-plt.figure(1).show()
 import pandas as pd
-import Tkinter
 
 class SmaCross(bt.SignalStrategy):
     def __init__(self):
         sma1, sma2 = bt.ind.SMA(period=10), bt.ind.SMA(period=30)
         crossover = bt.ind.CrossOver(sma1, sma2)
         self.signal_add(bt.SIGNAL_LONG, crossover)
-print(0)
+
+is_live = False
+
 cerebro = bt.Cerebro()
 cerebro.addstrategy(SmaCross)
-print(1)
-store = alpaca_backtrader_api.AlpacaStore(
-    key_id='PKTN99XWM1SQATZ28PBM',
-    secret_key='mvAMEH6usShrMzFYWtvlHT5v97OeC7eIQ5J6/QWn',
-    paper=True
-)
-print(2)
-broker = store.getbroker()  # or just alpaca_backtrader_api.AlpacaBroker()
-cerebro.setbroker(broker)
-print(3)
+
+store = alpaca_backtrader_api.AlpacaStore()
+
+if is_live:
+    broker = store.getbroker()  # or just alpaca_backtrader_api.AlpacaBroker()
+    cerebro.setbroker(broker)
+ else:
+    cerebro.broker.setcash(100000)
+    cerebro.broker.setcommission(commission=0.0)
+    cerebro.addsizer(bt.sizers.PercentSizer, percents=20)
+
 DataFactory = store.getdata # or use alpaca_backtrader_api.AlpacaData
-#data0 = DataFactory(dataname='AAPL', timeframe=bt.TimeFrame.TFrame("Days"))  # Supported timeframes: "Days"/"Minutes"
+if is_live:
 data0 = DataFactory(
     dataname='AAPL',
     timeframe=bt.TimeFrame.TFrame("Minutes"),
-    fromdate=pd.Timestamp('2018-11-15'),
-    todate=pd.Timestamp('2018-11-17'),
-    historical=True)
+    )
+else:
+    data0 = DataFactory(
+        dataname='AAPL',
+        timeframe=bt.TimeFrame.TFrame("Minutes"),
+        fromdate=pd.Timestamp('2018-11-15'),
+        todate=pd.Timestamp('2018-11-17'),
+        historical=True)
 cerebro.adddata(data0)
-cerebro.adddata(data0)
-print(4)
-cerebro.run(exactbars=-1) # or -1 or -2
-print("Final Portfolio Value: %.2f" % cerebro.broker.getvalue())
-cerebro.plot()[0][0]
-print(5)
+
+cerebro.run()
+print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+cerebro.plot()
