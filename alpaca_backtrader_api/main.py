595e26bc5be093430bc1e0b66223af193742fb3e
Alan Lee <Alan@Changyings-iMac.local>
2019-12-21 22:55:51 -0500

    fixed api key adds

--- alpaca_backtrader_api/main.py
+++ alpaca_backtrader_api/main.py
@@ -2,6 +2,10 @@ import backtrader as bt
 import alpaca_backtrader_api
 import pandas as pd
 
+API_KEY = "PKTN99XWM1SQATZ28PBM"
+API_SECRET = "mvAMEH6usShrMzFYWtvlHT5v97OeC7eIQ5J6/QWn"
+APCA_API_BASE_URL = "https://paper-api.alpaca.markets"
+
 class SmaCross(bt.SignalStrategy):
     def __init__(self):
         sma1, sma2 = bt.ind.SMA(period=10), bt.ind.SMA(period=30)
@@ -13,22 +17,25 @@ is_live = False
 cerebro = bt.Cerebro()
 cerebro.addstrategy(SmaCross)
 
-store = alpaca_backtrader_api.AlpacaStore()
-
+store = alpaca_backtrader_api.AlpacaStore(
+    key_id=API_KEY,
+    secret_key=API_SECRET,
+    paper=APCA_API_BASE_URL
+)
 if is_live:
     broker = store.getbroker()  # or just alpaca_backtrader_api.AlpacaBroker()
     cerebro.setbroker(broker)
- else:
+else:
     cerebro.broker.setcash(100000)
     cerebro.broker.setcommission(commission=0.0)
     cerebro.addsizer(bt.sizers.PercentSizer, percents=20)
 
 DataFactory = store.getdata # or use alpaca_backtrader_api.AlpacaData
 if is_live:
-data0 = DataFactory(
-    dataname='AAPL',
-    timeframe=bt.TimeFrame.TFrame("Minutes"),
-    )
+    data0 = DataFactory(
+        dataname='AAPL',
+        timeframe=bt.TimeFrame.TFrame("Minutes"),
+        )
 else:
     data0 = DataFactory(
         dataname='AAPL',
